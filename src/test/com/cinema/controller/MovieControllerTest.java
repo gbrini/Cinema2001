3d6b6a553a48ae8441131e1881ebb1d85bccfc6b
guido <guido.brini99@gmail.com>
2026-02-16 19:11:21 +0100

    inizio test movie

--- src/test/com/cinema/controller/MovieControllerTest.java
+++ src/test/com/cinema/controller/MovieControllerTest.java
@@ -1,4 +1,24 @@
 package test.com.cinema.controller;
 
+import com.cinema.controller.MovieController;
+import com.cinema.model.Movie;
+import com.cinema.service.auth.UserSession;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MovieControllerTest {
+    private Movie testMovie;
+
+    @BeforeEach
+    void setup() {
+        //testMovie = new Movie();
+        UserSession.getInstance().cleanSession();
+    }
+
+    @Test
+    void testControllerInitialization() {
+        MovieController controller = new MovieController(testMovie);
+        assertNotNull(controller.getView(), "Il pannello ci deve essere");
+    }
 }
