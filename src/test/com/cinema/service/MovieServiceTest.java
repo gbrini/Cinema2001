8b3b400faef3d2641a279a9ffa52227d3dea672c
guido <guido.brini99@gmail.com>
2026-02-21 15:12:27 +0100

    Exception permission added

--- src/test/com/cinema/service/MovieServiceTest.java
+++ src/test/com/cinema/service/MovieServiceTest.java
@@ -4,16 +4,29 @@ import com.cinema.controller.auth.LoginController;
 import com.cinema.model.Movie;
 import com.cinema.model.User;
 import com.cinema.service.MovieService;
+import com.cinema.util.UnauthorizedAccessException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MovieServiceTest {
+    @Test
+    void getAllMoviesSuccess() throws SQLException {
+        User user = LoginController.getInstance().login("admin@me.com", "password");
+        assertDoesNotThrow(MovieService::getAllMovies);
+    }
+
+    @Test
+    void getAllMoviesF() throws SQLException {
+        User user = LoginController.getInstance().login("admin@me.com", "password");
+        assertThrows(UnauthorizedAccessException.class, MovieService::getAllMovies);
+    }
 
     @Test
     void addMovie() throws SQLException {
