3c05cd5ddde228f9bee214c4f83af87004b45094
guido <guido.brini99@gmail.com>
2026-02-21 08:59:55 +0100

    Test service film

---
+++ src/test/com/cinema/service/MovieServiceTest.java
@@ -0,0 +1,15 @@
+package test.com.cinema.service;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MovieServiceTest {
+    @Test
+    void example() {
+        
+    }
+}
