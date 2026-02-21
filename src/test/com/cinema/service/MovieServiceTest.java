bbea3b0a00d5048df63472a71e5aacc63c50c88f
guido <guido.brini99@gmail.com>
2026-02-21 12:21:47 +0100

    Test service film init

--- src/test/com/cinema/service/MovieServiceTest.java
+++ src/test/com/cinema/service/MovieServiceTest.java
@@ -1,18 +1,24 @@
 package test.com.cinema.service;
 
+import com.cinema.controller.auth.LoginController;
+import com.cinema.model.Movie;
+import com.cinema.model.User;
 import com.cinema.service.MovieService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MovieServiceTest {
-    private MovieService movieService;
-    
-    @Test
-    void example() {
 
+    @Test
+    void addMovie() throws SQLException {
+        User user = LoginController.getInstance().login("admin@me.com", "password");
+        Movie movie = null;
+        MovieService.addMovie(movie);
     }
 }
