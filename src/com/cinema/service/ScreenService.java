638379865c1af587f93c28bfd013aee50bb8897b
guido <guido.brini99@gmail.com>
2026-02-25 18:02:50 +0100

    rev screen services permissions execptions

--- src/com/cinema/service/ScreenService.java
+++ src/com/cinema/service/ScreenService.java
@@ -2,14 +2,15 @@ package com.cinema.service;
 
 import com.cinema.model.Screen;
 import com.cinema.model.dao.ScreenDAO;
+import com.cinema.util.UnauthorizedAccessException;
 
 import java.util.ArrayList;
 
 public class ScreenService {
     public static int addScreen(Screen screen) {
-        if (PermissionService.hasPermission("screen:add"))
-            return ScreenDAO.addScreen(screen);
-        return -1;
+        if (!PermissionService.hasPermission("screen:add"))
+            throw new UnauthorizedAccessException("Accesso non consentito");
+        return ScreenDAO.addScreen(screen);
     }
 
     public static int getLatestScreenId() {
@@ -17,8 +18,8 @@ public class ScreenService {
     }
 
     public static ArrayList<Screen> getAllScreen() {
-        if (PermissionService.hasPermission("screen:view"))
-            return ScreenDAO.getAllScreen();
-        return new ArrayList<>();
+        if (!PermissionService.hasPermission("screen:view"))
+            throw new UnauthorizedAccessException("Accesso non consentito");
+        return ScreenDAO.getAllScreen();
     }
 }
