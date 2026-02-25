0c0bdbb6ec784bf098125082264eb1d1b598652e
guido <guido.brini99@gmail.com>
2026-02-25 18:04:38 +0100

    rev seat services permissions execptions

--- src/com/cinema/service/SeatService.java
+++ src/com/cinema/service/SeatService.java
@@ -3,32 +3,33 @@ package com.cinema.service;
 import com.cinema.model.Seat;
 import com.cinema.model.SeatEditor;
 import com.cinema.model.dao.SeatDAO;
+import com.cinema.util.UnauthorizedAccessException;
 
 import java.util.ArrayList;
 
 public class SeatService {
     public static boolean addSeats(ArrayList<Seat> seats) {
-        if (PermissionService.hasPermission("seat:add"))
-            return SeatDAO.addSeats(seats);
-        return false;
+        if (!PermissionService.hasPermission("seat:add"))
+            throw new UnauthorizedAccessException("Accesso non consentito");
+        return SeatDAO.addSeats(seats);
     }
 
     public static boolean upsertSeats(ArrayList<Seat> seats) {
-        if (PermissionService.hasPermission("seat:add") && PermissionService.hasPermission("seat:edit"))
-            return SeatDAO.upsertSeats(seats);
-        return false;
+        if (!PermissionService.hasPermission("seat:add") && PermissionService.hasPermission("seat:edit"))
+            throw new UnauthorizedAccessException("Accesso non consentito");
+        return SeatDAO.upsertSeats(seats);
     }
 
     public static boolean removeSeatsByScreenId(int screenId) {
-        if (PermissionService.hasPermission("seat:remove"))
-            return SeatDAO.removeSeatsByScreenId(screenId);
-        return false;
+        if (!PermissionService.hasPermission("seat:remove"))
+            throw new UnauthorizedAccessException("Accesso non consentito");
+        return SeatDAO.removeSeatsByScreenId(screenId);
     }
 
     public static ArrayList<Seat> getSeatsByScreenId(int screenId) {
-        if (PermissionService.hasPermission("seat:view"))
-            return SeatDAO.getSeatsByScreenId(screenId);
-        return new ArrayList<>();
+        if (!PermissionService.hasPermission("seat:view"))
+            throw new UnauthorizedAccessException("Accesso non consentito");
+        return SeatDAO.getSeatsByScreenId(screenId);
     }
 
     public static ArrayList<SeatEditor> getSeatsStatusByScreeningId(int screeningId) {
