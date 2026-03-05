31151374864daec116b5b921ce8277be9e5a4227
guido <guido.brini99@gmail.com>
2026-03-05 14:22:14 +0100

    env

---
+++ src/com/cinema/util/EnvConfig.java
@@ -0,0 +1,31 @@
+package com.cinema.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class EnvConfig {
+    private static EnvConfig instance;
+    private final Properties properties = new Properties();
+
+    private EnvConfig() {
+        var envFile = Paths.get(System.getProperty("user.home"), ".env", "config.properties");
+        try(var inputStream = Files.newInputStream(envFile)) {
+            properties.load(inputStream);
+        } catch (IOException exc) {
+            throw new RuntimeException(exc);
+        }
+    }
+
+    private EnvConfig getInstance() {
+        if (instance == null) {
+            instance = new EnvConfig();
+        }
+        return instance;
+    }
+
+    public String get(String key) {
+        return properties.getProperty(key);
+    }
+}
