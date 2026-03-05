df301478c0ce4d43c2a2612400e8da4972f88c02
guido <guido.brini99@gmail.com>
2026-03-05 14:39:32 +0100

    env

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
-import com.cinema.util.EnvConfig;
 import com.cinema.view.auth.LoginFrame;
 import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(EnvConfig.getInstance().get("test"));
-        System.out.println(EnvConfig.getInstance().get("password"));
         SwingUtilities.invokeLater(() -> {
             new LoginFrame().setVisible(true);
         });
