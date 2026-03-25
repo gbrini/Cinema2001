b08e8d1e56bec17ea8d26937089751e2fe879a67
guido <guido.brini99@gmail.com>
2026-03-25 18:33:10 +0100

    inizio biglietti rev

--- src/com/cinema/model/Ticket.java
+++ src/com/cinema/model/Ticket.java
@@ -3,13 +3,13 @@ package com.cinema.model;
 import java.time.LocalDate;
 
 public class Ticket {
-    private int ticketId;
-    private int screeningId;
-    private int typeId;
-    private int seatId;
-    private int userId;
-    private LocalDate purchaseTime;
-    private float finalPrice;
+    private final int ticketId;
+    private final int screeningId;
+    private final int typeId;
+    private final int seatId;
+    private final int userId;
+    private final LocalDate purchaseTime;
+    private final float finalPrice;
 
     private Ticket(Builder builder) {
         this.ticketId = builder.ticketId;
@@ -39,5 +39,33 @@ public class Ticket {
         public Builder setFinalPrice(float finalPrice) { this.finalPrice = finalPrice; return this; }
 
         public Ticket build() { return new Ticket(this); }
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public int getScreeningId() {
+        return screeningId;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public int getSeatId() {
+        return seatId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public LocalDate getPurchaseTime() {
+        return purchaseTime;
+    }
+
+    public float getFinalPrice() {
+        return finalPrice;
     }
 }
