d5393c17c1922598ed9f13f31a33bb3c9154b459
guido <guido.brini99@gmail.com>
2026-03-25 18:31:34 +0100

    inizio biglietti rev

--- src/com/cinema/model/Ticket.java
+++ src/com/cinema/model/Ticket.java
@@ -12,11 +12,31 @@ public class Ticket {
     private float finalPrice;
 
     private Ticket(Builder builder) {
-
+        this.ticketId = builder.ticketId;
+        this.screeningId = builder.screeningId;
+        this.typeId = builder.typeId;
+        this.seatId = builder.seatId;
+        this.userId = builder.userId;
+        this.purchaseTime = builder.purchaseTime;
+        this.finalPrice = builder.finalPrice;
     }
 
     public static class Builder {
+        private int ticketId;
+        private int screeningId;
+        private int typeId;
+        private int seatId;
+        private int userId;
+        private LocalDate purchaseTime;
+        private float finalPrice;
 
+        public Builder setTicketId(int ticketId) { this.ticketId = ticketId; return this; }
+        public Builder setScreeningId(int screeningId) { this.screeningId = screeningId; return this; }
+        public Builder setTypeId(int typeId) { this.typeId = typeId; return this; }
+        public Builder setSeatId(int seatId) { this.seatId = seatId; return this; }
+        public Builder setUserId(int userId) { this.userId = userId; return this; }
+        public Builder setPurchaseTime(LocalDate purchaseTime) { this.purchaseTime = purchaseTime; return this; }
+        public Builder setFinalPrice(float finalPrice) { this.finalPrice = finalPrice; return this; }
 
         public Ticket build() { return new Ticket(this); }
     }
