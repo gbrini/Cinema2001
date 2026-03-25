c94b9d190bd1ecd3ba9e39556044f3f9df8eb7d8
guido <guido.brini99@gmail.com>
2026-03-25 14:44:10 +0100

    inizio biglietti rev

--- src/com/cinema/model/TicketType.java
+++ src/com/cinema/model/TicketType.java
@@ -2,30 +2,30 @@ package com.cinema.model;
 
 public class TicketType {
     private final int typeId;
-    private final String  typeName;
+    private final String typeName;
     private final double baseDiscountPercent;
-    private final double basepriceAddendum;
-    
-    public TicketType(int typeId, String typeName, double baseDiscountPercent, double basepriceAddendum) {
+    private final double basePriceAddendum;
+
+    public TicketType(int typeId, String typeName, double baseDiscountPercent, double basePriceAddendum) {
         this.typeId = typeId;
         this.typeName = typeName;
         this.baseDiscountPercent = baseDiscountPercent;
-        this.basepriceAddendum = basepriceAddendum;
-    }
-
-    public String getTypeName() {
-        return typeName;
+        this.basePriceAddendum = basePriceAddendum;
     }
 
     public int getTypeId() {
         return typeId;
     }
 
+    public String getTypeName() {
+        return typeName;
+    }
+
     public double getBaseDiscountPercent() {
         return baseDiscountPercent;
     }
 
-    public double getBasepriceAddendum() {
-        return basepriceAddendum;
+    public double getBasePriceAddendum() {
+        return basePriceAddendum;
     }
 }
