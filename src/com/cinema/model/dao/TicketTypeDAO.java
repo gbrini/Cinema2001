b2e92c5e79b440d96f91b61e1d7f813a35947861
guido <guido.brini99@gmail.com>
2026-03-25 14:55:15 +0100

    inizio biglietti rev

---
+++ src/com/cinema/model/dao/TicketTypeDAO.java
@@ -0,0 +1,17 @@
+package com.cinema.model.dao;
+
+import com.cinema.model.TicketType;
+
+import java.util.ArrayList;
+
+public class TicketTypeDAO {
+    public static ArrayList<TicketType> getTicketTypes() {
+        ArrayList<TicketType> ticketTypes = new ArrayList<>();
+
+        return ticketTypes;
+    }
+
+    public static TicketType getTicketTypeById(int id) {
+        return null;
+    }
+}
