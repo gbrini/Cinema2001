905d5b25490ea03c81d2887ab11a402a582168f1
guido <guido.brini99@gmail.com>
2026-04-14 17:16:57 +0200

    rev

--- src/com/cinema/util/DialogCloseObserver.java
+++ src/com/cinema/util/DialogCloseObserver.java
@@ -1,8 +1,5 @@
 package com.cinema.util;
 
 public interface DialogCloseObserver {
-    /**
-     * Called by the subject (JDialog) when it's about to close
-     */
     void onDialogClosed(boolean changedSaved);
 }
